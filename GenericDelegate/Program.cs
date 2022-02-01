03cbe432f9a76fbf39b528ab6987dd7eb3cb8d49
GreedNeSS <ruslan199126@gmail.com>
2022-02-01 23:13:59 +0300

    Added generic delegate example

---
+++ GenericDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace GenericDelegate
+{
+    public delegate void MyGenericDelegate<T>(T arg);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyGenericDelegate<string> strTarget = Stringtarget;
+            MyGenericDelegate<int> intTarget = new (IntTarget);
+
+            strTarget("Hello");
+            IntTarget(20);
+        }
+
+        static void Stringtarget(string arg)
+        {
+            Console.WriteLine($"arg in UpperCase is: {arg.ToUpper()}");
+        }
+
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine($"++arg: {++arg}");
+        }
+    }
+}
