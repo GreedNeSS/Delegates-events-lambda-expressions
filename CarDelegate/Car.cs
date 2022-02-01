c2e301c03f7e26f36d3041c3a766c1bac5430ee5
GreedNeSS <ruslan199126@gmail.com>
2022-02-01 22:54:19 +0300

    Added new example

---
+++ CarDelegate/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDelegate
+{
+    class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string Petname { get; set; }
+        private bool carIsDead;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string name, int maxSp, int curSp)
+        {
+            Petname = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = curSp;
+        }
+
+        public delegate void CarEngineHandler(string message);
+        private CarEngineHandler listHandlers;
+
+        public void RegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listHandlers += methodToCall;
+        }
+
+        public void UnRegisterWithCarEngine(CarEngineHandler methodToCall)
+        {
+            listHandlers -= methodToCall;
+        }
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                if (listHandlers != null)
+                {
+                    listHandlers("Sorry this car is dead...");
+                }
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if (
+                    10 <= (MaxSpeed - CurrentSpeed) &&
+                    0 >= (MaxSpeed - CurrentSpeed) &&
+                    listHandlers != null
+                    )
+                {
+                    listHandlers("Careful buggy! Gonna blow!");
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    listHandlers($"Engine is dead. Current speed: {CurrentSpeed}");
+                }
+                else
+                {
+                    Console.WriteLine($"CurrentSpeed = {CurrentSpeed}");
+                }
+            }
+        }
+    }
+}
---
+++ CarDelegate/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CarDelegate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new("Oka", 100, 10);
+            car.RegisterWithCarEngine(new Car.CarEngineHandler(OnCarEngineEvent));
+            car.RegisterWithCarEngine(OnCarEngineEvent2);
+
+            for (int i = 0; i < 5; i++)
+            {
+                car.Accelerate(20);
+            }
+
+            car.UnRegisterWithCarEngine(OnCarEngineEvent2);
+
+            car.Accelerate(10);
+        }
+
+        public static void OnCarEngineEvent(string message)
+        {
+            Console.WriteLine("\n***** Message From Car Object *****");
+            Console.WriteLine($"=> {message}");
+            Console.WriteLine("***********************************\n");
+        }
+
+        public static void OnCarEngineEvent2(string message)
+        {
+            Console.WriteLine("\n=> " + message.ToUpper() + "\n");
+        }
+    }
+}
