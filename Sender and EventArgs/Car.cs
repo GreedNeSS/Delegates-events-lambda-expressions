3f743c9662ef317a313d6986427b43eb54e5945b
GreedNeSS <ruslan199126@gmail.com>
2022-02-02 21:31:53 +0300

    Use sender and eventArgs contract

---
+++ Sender and EventArgs/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sender_and_EventArgs
+{
+    class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string Petname { get; set; }
+        private bool carIsDead;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string name, int maxSp, int curSp)
+        {
+            Petname = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = curSp;
+        }
+
+        public delegate void CarEngineHandler(object sender, CarEventArgs e);
+        public event CarEngineHandler AboutToBlow;
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler Info;
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                Exploded?.Invoke(this, new("Sorry this car is dead..."));
+             }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if (10 >= (MaxSpeed - CurrentSpeed) &&
+                    0 <= (MaxSpeed - CurrentSpeed))
+                {
+                    AboutToBlow?.Invoke(this,
+                        new("Careful buggy! Gonna blow!"));
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    Exploded?.Invoke(this, 
+                        new($"Engine is dead. Current speed: {CurrentSpeed}"));
+                }
+                else
+                {
+                    Info?.Invoke(this, new($"CurrentSpeed = {CurrentSpeed}"));
+                }
+            }
+        }
+    }
+}
---
+++ Sender and EventArgs/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Sender_and_EventArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new("Lada", 100, 10);
+            car.Info += Car_Info;
+            car.AboutToBlow += Car_AboutToBlow;
+            car.Exploded += Car_Exploded;
+
+            for (int i = 0; i < 6; i++)
+            {
+                car.Accelerate(20);
+            }
+
+            car.Exploded -= Car_Exploded;
+
+            for (int i = 0; i < 6; i++)
+            {
+                car.Accelerate(20);
+            }
+        }
+
+        private static void Car_Info(object sender, CarEventArgs e)
+        {
+            ConsoleColor colorPrev = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            if (sender is Car car)
+            {
+                Console.WriteLine($"Car name: {car.Petname}. Info: {e.msg}");
+            }
+
+            Console.ForegroundColor = colorPrev;
+        }
+
+        private static void Car_AboutToBlow(object sender, CarEventArgs e)
+        {
+            ConsoleColor colorPrev = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            if (sender is Car car)
+            {
+                Console.WriteLine($"Car name: {car.Petname}. Warning: {e.msg}");
+            }
+
+            Console.ForegroundColor = colorPrev;
+        }
+
+        private static void Car_Exploded(object sender, CarEventArgs e)
+        {
+            ConsoleColor colorPrev = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            if (sender is Car car)
+            {
+                Console.WriteLine($"Car name: {car.Petname}. Error: {e.msg}");
+            }
+
+            Console.ForegroundColor = colorPrev;
+        }
+    }
+}
