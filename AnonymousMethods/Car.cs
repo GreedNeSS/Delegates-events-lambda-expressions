2000e4459b0a1b5fa347c1361d7eea40ba883458
GreedNeSS <ruslan199126@gmail.com>
2022-02-03 18:07:06 +0300

    Use anonymous methods

---
+++ AnonymousMethods/Car.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethods
+{
+    class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string Petname { get; set; }
+        private bool carIsDead;
+
+        public Car()
+        {
+
+        }
+
+        public Car(string name, int maxSp, int curSp)
+        {
+            Petname = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = curSp;
+        }
+
+        public event EventHandler<CarEventArgs> AboutToBlow;
+        public event EventHandler<CarEventArgs> Exploded;
+        public event EventHandler<CarEventArgs> Info;
+
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+            {
+                Exploded?.Invoke(this, new("Sorry this car is dead..."));
+             }
+            else
+            {
+                CurrentSpeed += delta;
+
+                if (10 >= (MaxSpeed - CurrentSpeed) &&
+                    0 <= (MaxSpeed - CurrentSpeed))
+                {
+                    AboutToBlow?.Invoke(this,
+                        new("Careful buggy! Gonna blow!"));
+                }
+
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    Exploded?.Invoke(this, 
+                        new($"Engine is dead. Current speed: {CurrentSpeed}"));
+                }
+                else
+                {
+                    Info?.Invoke(this, new($"CurrentSpeed = {CurrentSpeed}"));
+                }
+            }
+        }
+    }
+}
---
+++ AnonymousMethods/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace AnonymousMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new("Lada", 100, 10);
+
+            car.Info += delegate (object sender, CarEventArgs e)
+            {
+                ConsoleColor colorPrev = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                if (sender is Car car)
+                {
+                    Console.WriteLine($"Car name: {car.Petname}. Info: {e.msg}");
+                }
+
+                Console.ForegroundColor = colorPrev;
+            };
+
+            car.AboutToBlow += delegate (object sender, CarEventArgs e)
+            {
+                ConsoleColor colorPrev = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                if (sender is Car car)
+                {
+                    Console.WriteLine($"Car name: {car.Petname}. Warning: {e.msg}");
+                }
+
+                Console.ForegroundColor = colorPrev;
+            };
+
+            car.Exploded += delegate (object sender, CarEventArgs e)
+            {
+                ConsoleColor colorPrev = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+
+                if (sender is Car car)
+                {
+                    Console.WriteLine($"Car name: {car.Petname}. Error: {e.msg}");
+                }
+
+                Console.ForegroundColor = colorPrev;
+            };
+
+            for (int i = 0; i < 6; i++)
+            {
+                car.Accelerate(20);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 2; i++)
+            {
+                car.Accelerate(20);
+            }
+        }
+    }
+}
