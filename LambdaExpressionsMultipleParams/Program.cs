1e0bd75a6b4a35d9900efb06d28fef0b2347951a
GreedNeSS <ruslan199126@gmail.com>
2022-02-04 19:40:30 +0300

    Added lambda expressions with multiple parameters and no parameters

---
+++ LambdaExpressionsMultipleParams/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LambdaExpressionsMultipleParams
+{
+    class Program
+    {
+        public delegate string SimpleDelegate();
+        static void Main(string[] args)
+        {
+            SimpleMath math = new();
+
+            math.SetMathHandler((msg, result) =>
+            {
+                Console.WriteLine($"Message: {msg}, Result: {result}");
+            });
+
+            math.Add(10, 10);
+
+            SimpleDelegate simpleDelegate = new SimpleDelegate(() => "Hello World");
+            Console.WriteLine(simpleDelegate());
+        }
+    }
+}
---
+++ LambdaExpressionsMultipleParams/SimpleMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionsMultipleParams
+{
+    class SimpleMath
+    {
+        public delegate void MathMessage(string msd, int result);
+        private MathMessage mmDelegate;
+
+        public void SetMathHandler(MathMessage handler)
+        {
+            mmDelegate = handler;
+        }
+
+        public void Add(int arg1, int arg2)
+        {
+            mmDelegate?.Invoke("Adding has completed!", arg1 + arg2);
+        }
+    }
+}
